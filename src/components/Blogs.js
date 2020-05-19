cb691af4ee6a0e57ab7b2183f50997ab1dc39ad6
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-05-19 14:20:21 +0200

    Sorting algorythm by number of likes for blogs implemented

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Blog from './Blog';
+import sortByLikes from '../helpers/sortByLikes';
 
 const Blogs = ({ blogs, setBlogs, user }) => {
   return (
     <div>
       <h2>Blogs list</h2>
-      {blogs.map(blog => (
+      {[...blogs].sort(sortByLikes).map(blog => (
         <Blog
           key={blog.id}
           blog={blog}
